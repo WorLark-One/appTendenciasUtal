2a34065043f202dc711fc4a7ad464041ae8e5be9
Felipe Milla <felipe.milla@haulmer.com>
2021-05-12 12:04:45 -0400

    feat(arduino connection): v1.

--- Tendenciascomputacionales/app/src/main/java/com/example/tendenciascomputacionales/MainActivity.java
+++ Tendenciascomputacionales/app/src/main/java/com/example/tendenciascomputacionales/MainActivity.java
@@ -2,24 +2,171 @@ package com.example.tendenciascomputacionales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.ActivityNotFoundException;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    TextView grabar;
+    public TextView grabar, estadoArduino;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    public static String address = null;
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
+    public boolean activar;
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private ConnectedThread MyConexionBT;
+    Button btConectarArduino, btDesconectarArduino;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         grabar = (TextView) findViewById(R.id.txtGrabarVoz);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btConectarArduino = findViewById(R.id.btConectarArduino);
+        btDesconectarArduino = findViewById(R.id.btDesconectarArduino);
+        estadoArduino = (TextView) findViewById(R.id.estadoArduino);
+
+        this.verificarEstadoBT();
+
+        Set<BluetoothDevice> pairedDeveicesList = btAdapter.getBondedDevices();
+        for(BluetoothDevice pairedDevice : pairedDeveicesList){
+            if(pairedDevice.getName().equals("HC-05")){
+                address = pairedDevice.getAddress();
+            }
+        }
+
+        btConectarArduino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activar = true;
+                onResume();
+            }
+        });
+
+        btDesconectarArduino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    btSocket.close();
+                    estadoArduino.setText("Desconectado");
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private BluetoothSocket createBluetoothSocket (BluetoothDevice device) throws IOException {
+
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    private void verificarEstadoBT(){
+        if(btAdapter.isEnabled()){
+
+        }else{
+
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent,1);
+        }
+
+    }
+
+    public void onResume() {
+        super.onResume();
+
+
+        if (activar) {
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+            try {
+                btSocket = createBluetoothSocket(device);
+
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+            }
+            // Establece la conexión con el socket Bluetooth.
+            try {
+                btSocket.connect();
+            } catch (IOException e) {
+                try {
+                    btSocket.close();
+                } catch (IOException e2) {
+
+                }
+            }
+            MyConexionBT = new ConnectedThread(btSocket);
+            MyConexionBT.start();
+            estadoArduino.setText("Conectado");
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+
+                    bytes = mmInStream.read(buffer);
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Envia los datos obtenidos hacia el evento via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //Envio de trama
+        public void write(String input) {
+            try {
+                mmOutStream.write(input.getBytes());
+            } catch (IOException e) {
+                //si no es posible enviar datos se cierra la conexión
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 
     @Override
